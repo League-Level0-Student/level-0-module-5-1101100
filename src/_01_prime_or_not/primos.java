b6367d3211e1ef96e8bd88ed9808d68e94323828
League Student <league@iMac-14.attlocal.net>
2019-12-13 16:56:35 -0800

    finished prime or not prime

--- src/_01_prime_or_not/primos.java
+++ src/_01_prime_or_not/primos.java
@@ -1,5 +1,24 @@
 package _01_prime_or_not;
 
-public class primos {
+import javax.swing.JOptionPane;
 
+public class primos {
+public static void main(String[] args) {
+	String num = JOptionPane.showInputDialog("enter a number");
+	int i = 0;
+	for ( i = 2; i < Integer.parseInt(num); i++) {
+		
+
+	if (Integer.parseInt(num) % i == 0) {
+		JOptionPane.showMessageDialog(null, "that is not a prime number");
+		break;
+	}
+	}
+	 if (i == Integer.parseInt(num) ) 
+		{
+			JOptionPane.showMessageDialog(null, "that is a prime number1");
+		}
+		
+	}	
 }
+
